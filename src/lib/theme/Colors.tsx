b229aacc215bdd010646c2024766a3bc5c383816
MadLab <hello@madlab.design>
2019-06-01 03:32:06 +0200

    home page

---
+++ src/lib/theme/Colors.tsx
@@ -0,0 +1,44 @@
+export const Colors = {
+  primary: '#727BC0',
+  secondary: '#484B5D',
+  warning: '#f5a623',
+  success: '#64AD44',
+  danger: '#D43F3D',
+  titleText: '#929ADA',
+  text: '#666B88',
+
+  darkPrimary: '#3d444b',
+  white: '#DAE0F6',
+  whiteLight: '#ededed',
+
+  buttonText: '#464763',
+
+  // Background
+  primaryBG: '#272731',
+  secondaryBG: '#2E2E3A',
+  heroBG: '#1D1F28',
+  navBG: '#484B5E',
+  cardBG: '#484B5E',
+  cardFooter: '#2E2E3A',
+
+  // border
+  border: '#666B88',
+  // Black
+  black: '#000',
+  superDark: '#34495e',
+  blackOpacity54: 'rgba(0, 0, 0, 0.54)',
+
+  // Gray
+  grayLight: '#bfbfbf',
+  grayMid: '#4a4a4a',
+  grayDary: '#555555',
+  bgGray: '#fcfcfd',
+  borderGray: '#eaeaea',
+
+  // BoxShadow
+  boxShadowBlack24: 'rgba(0, 0, 0, 0.24)',
+  boxShadowBlack32: 'rgba(164, 152, 135, 0.32)',
+  boxShadowPrimary: '0 -3px 16px 0 rgba(0, 0, 0, 0.04)',
+
+  madlabPrimary: '#ec008c',
+};
