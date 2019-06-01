411c67e324524359856d5450adf2a936cfa2f1a6
MadLab <hello@madlab.design>
2019-06-01 17:56:06 +0200

    preloaderand routers

---
+++ src/components/header/Header.style.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  padding: 0.8rem;
+  position: fixed;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.scss';
-import App from './pages/home/App';
-import * as serviceWorker from './serviceWorker';
+import Home from './pages/home/App';
+import Header from './components/header/Header';
+import Loader from './components/loader/Loader';
 
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => this.setState({ loading: false }), 1500);
+  }
+  render() {
+    const { loading } = this.state;
+    return (
+      <Loader loading={loading}>
+        <Router>
+          <Header />
+          <Home />
+        </Router>
+      </Loader>
+    );
+  }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
